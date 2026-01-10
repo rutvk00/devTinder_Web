921498bf6585a389c73ea48882ed3a88af4622aa
rutvk00 <prajapatirutvik107@gmail.com>
2026-01-10 20:43:53 +0530

    add Routes  and also  Footer

---
+++ src/components/Body.jsx
@@ -0,0 +1,16 @@
+
+import NavBar from './Navbar';
+import Footer from "./Footer";
+import { Outlet } from 'react-router-dom'
+
+const Body = () => {
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+export default Body
