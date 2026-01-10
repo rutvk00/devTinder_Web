45ce5475464009359979cef85c5097e99f4b3951
rutvk00 <prajapatirutvik107@gmail.com>
2026-01-10 22:03:01 +0530

    Complete login functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,83 @@
-import React from 'react'
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
 const Login = () => {
-  return (
-    <div>
-      Login Page
-    </div>
-  )
-}
+    const [emailId, setEmailId] = useState("rutvik@gmail.com");
+    const [password, setPassword] = useState("Rutvik@123");
+    const navigate = useNavigate();
+    const [error, setError] = useState("");
 
-export default Login
+
+    const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      console.log(res);
+      return navigate("/");
+    } catch (err) {
+      setError(err?.response?.data || "Something went wrong");
+    }
+  };
+
+    return (
+        <div className="flex justify-center my-10">
+            <div className="card bg-base-300 w-96 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title justify-center">
+                        Login
+                    </h2>
+                    <div>
+                        <label className="form-control w-full max-w-xs my-2">
+                        <div className="label">
+                            <span className="label-text">Email ID:</span>
+                        </div>
+                        <input
+                            type="text"
+                            value={emailId}
+                            className="input input-bordered w-full max-w-xs"
+                            onChange={(e) => setEmailId(e.target.value)}
+                        />
+                        </label>
+                        <label className="form-control w-full max-w-xs my-2">
+                        <div className="label">
+                            <span className="label-text">Password</span>
+                        </div>
+                        <input
+                            type="password"
+                            value={password}
+                            className="input input-bordered w-full max-w-xs"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        </label>
+                    </div>
+                    <p className="text-red-500">{error}</p>
+                    <div className="card-actions mt-4">
+                        <button
+                            onClick={handleLogin}
+                            className="
+                            btn btn-primary w-full
+                            text-base font-semibold tracking-wide
+                            rounded-xl
+                            shadow-lg
+                            hover:shadow-xl
+                            transition-all duration-200
+                            hover:scale-[1.02]
+                            active:scale-95
+                            "
+                        >
+                            Login
+                        </button>
+                        </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Login;
